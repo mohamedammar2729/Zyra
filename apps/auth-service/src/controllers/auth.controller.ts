bd07cadc6679127c64e91ca06ff5e307997ed565
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-10-05 13:48:26 +0300

    01: init basics of auth service

---
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -0,0 +1,168 @@
+import { NextFunction, Request } from "express";
+import { Response } from "express";
+import {
+  checkOtpRestictions,
+  sendOtp,
+  trackOtpRequest,
+  validateRegistrationData,
+  verifyOtp,
+} from "../utils/auth.helper";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "@packages/libs/prisma";
+import { AuthError, ValidationError } from "@packages/error-handler";
+import { setCookie } from "../utils/cookies/setCookie";
+
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    validateRegistrationData(req.body, "user");
+    const { name, email } = req.body;
+
+    const existingUser = await prisma.users.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return next(new ValidationError("User already exists with this email!"));
+    }
+
+    await checkOtpRestictions(email, next);
+    await trackOtpRequest(email, next);
+    await sendOtp(name, email, "user-activation-mail");
+
+    res
+      .status(200)
+      .json({ message: "OTP sent to email. Please verify your account." });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// verify user with otp
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, otp, password, name } = req.body;
+
+    if (!email || !otp || !password || !name) {
+      return next(new ValidationError("All fields are required!"));
+    }
+
+    const existingUser = await prisma.users.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return next(new ValidationError("User already exists with this email!"));
+    }
+
+    await verifyOtp(email, otp, next);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await prisma.users.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res
+      .status(201)
+      .json({ success: true, message: "User registered successfully!" });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// login user
+
+export const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new ValidationError("Email and password are required!"));
+    }
+    const user = await prisma.users.findUnique({
+      where: { email },
+    });
+
+    if (!user) return next(new AuthError("User doesn't exist!"));
+
+    // verify password
+    const isPasswordValid = await bcrypt.compare(password, user.password!);
+    if (!isPasswordValid) {
+      return next(new AuthError("Invalid email or password!"));
+    }
+
+    // Generate access and refresh tokens
+    const accessToken = jwt.sign(
+      { id: user.id, role: "user" },
+      process.env.ACCESS_TOKEN_SECRET as string,
+      { expiresIn: "15m" }
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user.id, role: "user" },
+      process.env.REFRESH_TOKEN_SECRET as string,
+      { expiresIn: "7d" }
+    );
+
+    // Store the refresh and access tokens in an httpOnly secure cookie
+    setCookie(res, "refreshToken", refreshToken);
+    setCookie(res, "accessToken", accessToken);
+
+    res
+      .status(200)
+      .json({
+        message: "Login successful!",
+        user: { id: user.id, email: user.email, name: user.name },
+      });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// user forgot password
+export const forgotPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return next(new ValidationError("Email is required!"));
+    }
+
+    const user = await prisma.users.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return next(new AuthError("User doesn't exist!"));
+    }
+
+    await checkOtpRestictions(email, next);
+    await trackOtpRequest(email, next);
+    await sendOtp(user.name, email, "password-reset");
+
+    res
+      .status(200)
+      .json({ message: "OTP sent to email. Please verify your account." });
+  } catch (error) {
+    return next(error);
+  }
+};
---
+++ packages/libs/redis/index.ts
@@ -0,0 +1,7 @@
+import Redis from 'ioredis';
+
+
+
+const redis = new Redis(process.env.REDIS_DATABASE_URL!);
+
+export default redis;
